c4c8d015d64075162405d340d7c1881eecabdf9b
Anas Alhwid <git.acquire952@passmail.net>
2026-03-07 13:19:51 +0300

    Executions History

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,3 +1,4 @@
+import { createId } from "@paralleldrive/cuid2";
 import toposort from "toposort";
 import type { Connection, Node } from "@/generated/prisma/client";
 import { inngest } from "./client";
@@ -57,5 +58,6 @@ export const sendWorkflowExecution = async (data: {
   return inngest.send({
     name: "workflows/execute.workflow",
     data,
+    id: createId(),
   });
 };
